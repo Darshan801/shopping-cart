8c4ab147ebd272d44fa085c22942578287b77135
Darshan801 <darshankunwar79@gmail.com>
2026-01-19 21:07:57 +0545

    model created in the app(category)

--- category/models.py
+++ category/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+class Category(models.Model):
+    category_name= models.CharField(max_length=50,unique=True)
+    # slug is basically an url
+    slug =models.CharField(max_length=500,unique=True)
+    description=models.TextField(max_length=500, blank=True)
+    car_image = models.ImageField(upload_to='photos/category', blank=True)
+
+    class Meta:
+        verbose_name='category'
+        verbose_name_plural='categories'
+    
+    def __str__(self):
+        return self.category_name
